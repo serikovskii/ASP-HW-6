8e3eab410e7d7c18ba3da474fb6275111e55c4b0
Azat <46015824+serikovskii@users.noreply.github.com>
2019-11-13 14:35:36 +0600

    modify view

--- WebApplication11/WebApplication11/Controllers/HomeController.cs
+++ WebApplication11/WebApplication11/Controllers/HomeController.cs
@@ -46,6 +46,48 @@ namespace WebApplication11.Controllers
             return View(product);
         }
 
+        public async Task<ActionResult> Edit(Guid? id)
+        {
+            Product product = await context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Name,Price,Description,Quantity")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Entry(product).State = EntityState.Modified;
+                await context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        public async Task<ActionResult> Delete(Guid? id)
+        {
+            Product product = await context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteProduct(Guid id)
+        {
+            Product product = await context.Products.FindAsync(id);
+            context.Products.Remove(product);
+            await context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
         protected override void Dispose(bool disposing)
         {
             if (disposing)
